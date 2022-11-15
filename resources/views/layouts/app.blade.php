9799b4ae109f7afc349f3ca997537f52acaf65c0
Powppo <mafrizal575@gmail.com>
2022-11-15 22:49:33 +0700

    2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,7 +86,7 @@
 
                                 @if(Auth::user()->image)
                                     <img class="image rounded-circle" src="{{asset('/storage/images/'.Auth::user()->image)}}" alt="profile_image" style="width: 80px;height: 80px; padding: 10px; margin: 0px; ">
-                                @endif
+                                @endif 
                                 
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link text-black dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
