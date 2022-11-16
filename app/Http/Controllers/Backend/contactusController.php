16fa96b1ab675b45d6e923ebd3d264b174f2fadb
Powppo <mafrizal575@gmail.com>
2022-11-16 10:29:49 +0700

    Contact_Us Setup

---
+++ app/Http/Controllers/Backend/contactusController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\contactus;
+
+class contactusController extends Controller
+{
+    public function upload(){
+        
+    }
+}
